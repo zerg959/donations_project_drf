af0aa0a8a5caab4c0be7c0c3a6b3169ded0b053a
zerg959 <zerg959@yandex.ru>
2025-09-10 17:14:19 +0300

    COLLECT class

--- project/models.py
+++ project/models.py
@@ -1,17 +1,18 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 from django.contrib.auth.models import User
-from django.db import models
 from django.core.mail import send_mail
 from django.conf import settings
 from django.utils import timezone
 
 class Payment(models.Model):
     """
-    Payment model.
+    Payment model: allows create single payments objs.
 
     Fields:
         - `id` (integer): unique payment id.
         - `user` (string): user who makes the payment.
+        - `collect` (string): payment purpose.
         - `amount` (number): payment amount (two decimal places).
         - `timestamp` (string, datetime): payment time (ISO format).
 
@@ -21,6 +22,7 @@ class Payment(models.Model):
         "id": 1,
         "user": "Homer Simpson",
         "amount": 500.00,
+        "collect": "Wedding",
         "timestamp": "2025-04-05T10:30:00Z"
     }
     ```
@@ -45,6 +47,37 @@ class Payment(models.Model):
 
 class Collect(models.Model):
     """
+    Collect model.
+
+    Fields:
+        - `id` (integer): unique collect id.
+        - `author` (string): user who start the collect.
+        - `title` (string): short name of the collect.
+        - `purpose` (string): collection purpose (one from the PURPOSE_CHOICES).
+        - `description` (string): collect description.
+        - `target_amount` (number): target amount.
+        - `current_amount` (number): collected amount.
+        - `participants` (number): unique participants.
+        - `created_at` (string, datetime): collect start.
+        - `ended_at` (string, datetime): collect end.
+        - `image` (image): collect image.
+
+    Example:
+        ```json
+            {
+            "id": 12345,
+            "author": "homer_simpson",
+            "title": "Lisa's wedding",
+            "purpose": "Wedding",
+            "description": "Collect on Lisa's wedding",
+            "target_amount": 50000.0,
+            "current_amount": 32750.0,
+            "participants": 148,
+            "created_at": "2023-10-01T09:30:00Z",
+            "ended_at": "2023-12-31T23:59:59Z",
+            "image": "/media/collects/lisas_wedding.jpg"
+            }
+        ```
     """
     PURPOSE_CHOICES = [
         ('birth', 'Birthday'),
@@ -71,8 +104,20 @@ class Collect(models.Model):
         decimal_places=2,
         default=0
         )
-    current_amount = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default=0
+    participants = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
+    image = models.ImageField(
+        upload_to='collections/',
+        null=True,
+        blank=True
         )
+    class Meta:
+        ordering = ['-created_at', 'ended_at', 'target_amount']
+    
+    def __str__(self):
+        return f"Collect: {self.title} by {self.author} for {self.target_amount}"
+    
+    def add_payment(self, user, amount):
+        with transaction.atomic():
+            collect = Collect.objects.select_for_update().get(pk=self.pk)
