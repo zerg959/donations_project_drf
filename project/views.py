aa855575f9fe8665c15a0b3e6e4ee36135f41f3f
zerg959 <zerg959@yandex.ru>
2025-09-11 12:49:34 +0300

    Collect and Payment viewsets

--- project/views.py
+++ project/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from project.models import Collect, Payment
+from project.serializers import CollectSerializer, PaymentSerializer, UserSerializer
 
-# Create your views here.
+class CollectViewSet(viewsets.ModelViewSet):
+    queryset = Collect.objects.all()
+    serializer_class = CollectSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['get'], url_path='feed')
+    def payments_feed(self, request, pk=None):
+        collect = self.get_object()
+        payments = collect.payments.all().order_by('-timestamp')
+        serializer = PaymentSerializer(payments, many=True)
+        return Response(serializer.data )
+    
+    @action(detail=True, methods=['post'], url_path='pay')
+    def pay(self, request, pk=None):
+        collect = self.get_object()
+        amount = request.data.get('amount')
+        try:
+            amount = float(amount)
+            if amount < 0:
+                raise ValueError
+            
+        except(ValueError, TypeError):
+            return Response(
+                {"err": "Amount must be a positive integer"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        payment = collect.add_payment(request.user, amount)
+        serializer = PaymentSerializer(payment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class PaymentViewSet(viewsets.ReadOmlyViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
